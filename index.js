bd2d60f3a7f85ffe77cc3f13c32a33c21f8b6bd2
Alice Genkin <agenkin1@gmail.com>
2021-09-19 18:59:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 // Write your solution here!
-const append = ["Milo", "Otis", "Garfield"]
-const prepend = ["Milo", "Otis", "Garfield"]
-const removeLast = ["Milo", "Otis", "Garfield"]
-const removeFirst = ["Milo", "Otis", "Garfield"]
+//const append = ["Milo", "Otis", "Garfield"]
+// const prepend = ["Milo", "Otis", "Garfield"]
+// const removeLast = ["Milo", "Otis", "Garfield"]
+// const removeFirst = ["Milo", "Otis", "Garfield"]
+const drivers  = ["Milo", "Otis", "Garfield"]
 
 
-drivers.push("Odie");
-drivers.prepend("Odie");
-removeLast.pop();
-removeFirst.shirt();
+function destructivelyAppendDriver(name) {
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+  return drivers
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift()
+  return drivers
+}
+
+
+function appendDriver(name) {
+  const newArray = [...drivers,name]
+return newArray
+}
+
+function prependDriver(name) {
+  const newArray = [name,...drivers]
+  return newArray
+}
+
+function removeLastDriver() {
+let newArray = [...drivers]
+newArray.pop()
+return newArray
+
+}
+
+function removeFirstDriver() {
+  let newArray = [...drivers]
+  newArray.shift()
+  return newArray
+
+}
